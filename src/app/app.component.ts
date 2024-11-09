f2de6cc8acddddd5392ff9868d3a6b78337c2f37
Fabian Armando Rodriguez Gomez <21000844@alumnos.utleon.edu.mx>
2024-11-08 21:08:26 -0600

    eliminar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 //import { TemahComponent } from './tem/temah/temah.component';
 import { TemapComponent } from './tem/temap/temap.component';
+import { AddmessageComponent } from "./tem/addmessage/addmessage.component";
+import { NavegadorComponent } from './navegador/navegador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TemapComponent],
+  imports: [RouterOutlet,NavegadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/utl/utl.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from "@angular/router";
+export default[
+    {
+        path: 'listaalumnos',
+        loadComponent:()=>import('./alumnos/alumnos.component')
+    },
+    {
+        path:'agregar',
+        loadComponent:()=>import('./agregar/agregar.component')
+    }
+]
